4e109d2c86bd20e621674e51885a7b36dcd98d82
cyycyy77 <yoonyoul0710@gmail.com>
2025-05-17 02:35:18 +0900

    Feat : 관리자 권한일 때만 댓글 수정/삭제 기능 접근 허용

--- src/main/java/com/apple/shop/comment/CommentController.java
+++ src/main/java/com/apple/shop/comment/CommentController.java
@@ -9,13 +9,11 @@ import jakarta.persistence.Id;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -37,5 +35,33 @@ public class CommentController {
         return "redirect:/detail/" + parentId;
     }
 
+    @GetMapping("/edit/comment/{id}")
+    public String editComment(@PathVariable Long id, Model model) {
+        Optional<Comment> result = commentRepository.findById(id);
+
+        if (result.isPresent()) {
+            model.addAttribute("data", result.get());
+            return "editcomment.html";
+        } else{
+            return "redirect:/list";
+        }
+
+    }
+
+    @PostMapping("/edit/comment")
+    public String editComment(@RequestParam Long id,
+                              @RequestParam String username,
+                              @RequestParam String content,
+                              @RequestParam Long parentId) {
+
+        commentService.editComment(id, username, content, parentId);
+        return "redirect:/list";
+    }
+
+    @DeleteMapping("/comment")
+    ResponseEntity<String> deleteComment(@RequestParam Long id){
+        commentRepository.deleteById(id);
+        return ResponseEntity.status(200).body("삭제완료");
+    }
 
 }
